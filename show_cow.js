f4e68b0e0ad9253c7b301cfbe1f38328e9ec975e
Alex Ellis <alexellis2@gmail.com>
2016-04-24 22:57:59 +0100

    Cow variable

--- show_cow.js
+++ show_cow.js
@@ -1,12 +1,13 @@
 var cows = require('./')
-
 var all = cows();
-if(!process.argv[2] || isNaN(Number(process.argv[2]))) {
+
+var cowArgument = process.argv[2]
+if(!cowArgument || isNaN(Number(cowArgument))) {
   console.log("Give cow number as argument");
 }
 else {
-  var cowNumber = Number(process.argv[2]);
-  if(cowNumber <= all.length) {
+  var cowNumber = Number(cowArgument);
+  if(cowNumber && cowNumber <= all.length) {
     console.log(all[cowNumber-1]);
   } else {
     console.log("Enter cow number between [1-"+all.length+"]")
