ac6ac88d1c1b1546e96a62b4beb65fed4bf9dc75
Matt Thompson <whatknight@gmail.com>
2018-01-05 11:46:38 -0500

    actually just ignore var so it builds in ye olde versions of node

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
-const fs = require('fs');
-const path = require('path');
+/* eslint no-var: "off" */
+var fs = require('fs');
+var path = require('path');
 
 module.exports = function () {
 	return fs.readFileSync(path.join(__dirname, 'cows.txt'), 'utf8')
