43a6fd8bac37d4c6c03250d670a71daf4da3d0f8
Matt Thompson <whatknight@gmail.com>
2018-01-05 11:39:05 -0500

    lint roll

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function () {
 	return fs.readFileSync(path.join(__dirname, 'cows.txt'), 'utf8')
